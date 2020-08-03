b340497763ae8c8fd56b8c40a3db48e33307109f
Kapadokia-Titus <ktnyandoch@gmail.com>
2020-08-04 00:55:56 +0300

    working on netork call

--- app/src/main/java/kapadokia/nyandoro/cinema/ApiUtil.java
+++ app/src/main/java/kapadokia/nyandoro/cinema/ApiUtil.java
@@ -1,8 +1,13 @@
 package kapadokia.nyandoro.cinema;
 
 import android.net.Uri;
+import android.util.Log;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Scanner;
 
 public class ApiUtil {
 
@@ -26,5 +31,41 @@ public class ApiUtil {
         }
 
         return url;
+    }
+
+    // connecting to the Api
+    public static String getJson(URL url) throws IOException{
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        try {
+            InputStream inputStream = connection.getInputStream();
+            Scanner scanner =new Scanner(inputStream);
+
+            //if we want to read everything we set the delimeter to \\A
+            // the \\A is a pattern which is a regular expression.
+            scanner.useDelimiter("\\A");
+
+            // checking if the url has data
+            // true if it has, and false if it does'nt
+            boolean hasData = scanner.hasNext();
+
+            // check if data exists, hence return null.
+            if (hasData == true){
+                return scanner.next();
+            }else {
+                return null;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            Log.d("error", e.toString());
+            return null;
+        }
+
+        //finally close the connection calling the disconnect() method
+        finally {
+            connection.disconnect();
+        }
+
     }
 }
